485e8773cf18742f90674d67f6ca1098a8f569e6
Andres Ortiz <63428778+jaortiz16@users.noreply.github.com>
2023-11-20 20:18:40 -0500

    Update constantes.php

--- constantes.php
+++ constantes.php
@@ -1,6 +1,8 @@
 <?php
-  define("SERVER","localhost");
+  define("SERVER","viaduct.proxy.rlwy.net");
   define("USER","root");
-  define("PASS","123");
-  define("BD","clinicamedica");
+  define("PASS","d4e6ahgDH12-bGhA416Gca1h6C44g4fg");
+  define("BD","railway");
+  define("PORT","17862");
+
  ?>
